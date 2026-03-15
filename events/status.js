8c4d4efd4f1f732bb25ac8df0e87b729d1c99a6d
ketchupcomponent <101750090+iHategithub9000@users.noreply.github.com>
2026-03-15 15:32:18 +0100

    Update status.js

--- events/status.js
+++ events/status.js
@@ -1,11 +1,10 @@
-const { PresenceUpdateStatus } = require("discord.js");
 
 module.exports = {
   type: "ready",
   runOnce: true,
   run: async (client) => {
     client.user.setPresence({
-      status: PresenceUpdateStatus.Online,
+      status: "online",
       activities: [
         {
           type: 4,
