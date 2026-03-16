16196fb6641effcc024bff03cbc7c8205773a9ae
ketchupcomponent <101750090+iHategithub9000@users.noreply.github.com>
2026-03-16 17:16:10 +0100

    Rename action to otaku in command module

---
+++ commands/otaku.js
@@ -0,0 +1,56 @@
+const { RestrictionsEnum } = require("../commandAccessRestrictions.js");
+const { EmbedBuilder } = require("discord.js");
+
+const fetch = (...args) => import("node-fetch").then(({ default: fetch }) => fetch(...args));
+
+const reactions = {
+    hug: { verb: "hugs" },
+    kiss: { verb: "kisses" },
+    blush: { verb: "blushes", solo: "is blushing" },
+    clap: { verb: "claps for", solo: "claps" },
+    cry: { verb: "cries with", solo: "cries" },
+    facepalm: { verb: "facepalms at", solo: "facepalms" },
+    handhold: { verb: "holds hands with" },
+    happy: { verb: "is happy with", solo: "is happy" },
+    no: { verb: "says no to", solo: "says no" },
+    yes: { verb: "says yes to", solo: "says yes" },
+    pat: { verb: "pats" },
+    scared: { verb: "is scared of", solo: "is scared" },
+    tickle: { verb: "tickles" }
+};
+
+module.exports = {
+    accessRestriction: RestrictionsEnum.NONE,
+    accessRestrictionArgs: "",
+    name: "otaku",
+    help_string: "<action> [user] - anime reaction",
+
+    run: async (msg, argv, cl) => {
+
+        const action = argv[0];
+        const data = reactions[action];
+
+        if (!data) {
+            return msg.reply(`Actions: ${Object.keys(reactions).join(", ")}`);
+        }
+
+        const target = msg.mentions.users.first();
+
+        const res = await fetch(`https://api.otakugifs.xyz/gif?reaction=${action}`);
+        const gif = await res.json();
+
+        let text;
+
+        if (target) {
+            text = `**${msg.author.username}** ${data.verb} **${target.username}**`;
+        } else {
+            text = `**${msg.author.username}** ${data.solo || data.verb}`;
+        }
+
+        const embed = new EmbedBuilder()
+            .setDescription(text)
+            .setImage(gif.url);
+
+        msg.reply({ embeds: [embed] });
+    }
+};
