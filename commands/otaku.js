a8db451e657d9e8b75ddd0ed2e9deb3052c41919
ketchupcomponent <101750090+iHategithub9000@users.noreply.github.com>
2026-03-16 17:22:19 +0100

    Enhance reactions with solo messages
    Added solo responses for hug, kiss, handhold, pat, and tickle reactions.

--- commands/otaku.js
+++ commands/otaku.js
@@ -4,19 +4,19 @@ const { EmbedBuilder } = require("discord.js");
 const fetch = (...args) => import("node-fetch").then(({ default: fetch }) => fetch(...args));
 
 const reactions = {
-    hug: { verb: "hugs" },
-    kiss: { verb: "kisses" },
+    hug: { verb: "hugs", solo: "wants to be hugged" },
+    kiss: { verb: "kisses", solo: "wants to be kissed" },
     blush: { verb: "blushes", solo: "is blushing" },
     clap: { verb: "claps for", solo: "claps" },
     cry: { verb: "cries with", solo: "cries" },
     facepalm: { verb: "facepalms at", solo: "facepalms" },
-    handhold: { verb: "holds hands with" },
+    handhold: { verb: "holds hands with", solo: "holds hands with someone"},
     happy: { verb: "is happy with", solo: "is happy" },
     no: { verb: "says no to", solo: "says no" },
     yes: { verb: "says yes to", solo: "says yes" },
-    pat: { verb: "pats" },
+    pat: { verb: "pats", solo: "wants to be pet" },
     scared: { verb: "is scared of", solo: "is scared" },
-    tickle: { verb: "tickles" }
+    tickle: { verb: "tickles", solo: "wants to be tickled" }
 };
 
 module.exports = {
