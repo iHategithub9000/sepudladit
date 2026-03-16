cc2c88eed14b1f8e1a425f8212c48788fdec0060
ketchupcomponent <101750090+iHategithub9000@users.noreply.github.com>
2026-03-16 16:29:32 +0100

    Rename command from 'invite' to 'repo'

--- commands/repo.js
+++ commands/repo.js
@@ -5,7 +5,7 @@ const { EmbedBuilder } = require('discord.js');
 module.exports = {
     accessRestriction: RestrictionsEnum.NONE,
     accessRestrictionArgs: "",
-    name: "invite",
+    name: "repo",
     help_string: "- Sends bot GitHub repository link",
     run: async (msg, argv, cl) => {
         msg.reply(`https://github.com/iHategithub9000/kstetodc`)
