747f151e15ae48cecd34d69a602741b565951d99
ketchupcomponent <101750090+iHategithub9000@users.noreply.github.com>
2026-03-17 21:40:10 +0100

    Increase purge message limit from 10 to 100

--- commands/purge.js
+++ commands/purge.js
@@ -26,7 +26,7 @@ module.exports = {
             .setTitle(":broom: Purge completed.")
             .setFooter({text:"Purge requested by "+auth})
             .setColor(0x00ff00)
-            for (let i = 0; i < 10; i++) {
+            for (let i = 0; i < 100; i++) {
               await ch.send(":broom:"+("\n".repeat(200))+":broom:")
             }
             await ch.send({embeds:[embed]})
