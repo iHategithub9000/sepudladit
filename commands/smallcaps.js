ee3e3721925f221ab1a0d79dc74e0171d77eb286
ketchupcomponent <101750090+iHategithub9000@users.noreply.github.com>
2026-03-17 17:24:19 +0100

    Implement smallcaps function for text transformation

--- commands/smallcaps.js
+++ commands/smallcaps.js
@@ -1,1 +1,29 @@
+const { RestrictionsEnum } = require("../commandAccessRestrictions.js");
+const { EmbedBuilder, PresenceUpdateStatus } = require('discord.js');
 
+function toSmallCaps(text) {
+  const map = {
+    a: "ᴀ", b: "ʙ", c: "ᴄ", d: "ᴅ", e: "ᴇ",
+    f: "ꜰ", g: "ɢ", h: "ʜ", i: "ɪ", j: "ᴊ",
+    k: "ᴋ", l: "ʟ", m: "ᴍ", n: "ɴ", o: "ᴏ",
+    p: "ᴘ", q: "ꞯ", r: "ʀ", s: "s", t: "ᴛ",
+    u: "ᴜ", v: "ᴠ", w: "ᴡ", x: "x", y: "ʏ",
+    z: "ᴢ"
+  };
+
+  return text
+    .toLowerCase()
+    .split("")
+    .map(char => map[char] || char)
+    .join("");
+}
+
+module.exports = {
+    accessRestriction: RestrictionsEnum.NONE,
+    accessRestrictionArgs: "",
+    name: "smallcaps",
+    help_string: "<string> - smallcapsifies string. pretty!",
+    run: async (msg, argv, cl) => {
+        msg.reply(toSmallCaps(argv.slice(1).join("")))
+    }
+}
