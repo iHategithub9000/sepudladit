aa3896458302aee08bfe2881a6563b21649a5a28
ketchupcomponent <101750090+iHategithub9000@users.noreply.github.com>
2026-04-02 08:12:11 +0200

    Update leave_unknown_guilds2.js

--- events/leave_unknown_guilds2.js
+++ events/leave_unknown_guilds2.js
@@ -3,7 +3,7 @@ module.exports = {
     runOnce: true,
     run: async (_, cl) => {
         cl.guilds.cache.forEach(async g => {
-            if (g.id == "1463989244526723247" || g.id == "1272960100751114240") return;
+            if (g.id == "1463989244526723247" || g.id == "1272960100751114240" || g.id == "1489144963374776431") return;
             await g.leave();
         });
     }
